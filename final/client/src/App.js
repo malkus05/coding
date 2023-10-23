27315d214ab04a2a76f1bf07d96a3362f9ff5624
malkus05 <maicol.bautista@gmail.com>
2023-10-22 21:28:58 -0500

    back-front y Get

--- final/client/src/App.js
+++ final/client/src/App.js
@@ -1,65 +1,148 @@
 import './App.css';
-import { useState, usestate } from "react"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App (){
-  
-  const[Nombre, setNombre]= useState("")
-  const[Edad, setEdad]= useState("0")
-  const[Departamento, setDepartamento]= useState("")
-  const[Cargo, setCargo]= useState("")
-  const[Fecha_Ingreso, setfecha_Ingreso]= useState("")
-  const[Antiguedad, setAntiguedad]= useState("0")
+function App() {
+  const [Nombre, setNombre] = useState("");
+  const [Edad, setEdad] = useState(0);
+  const [Departamento, setDepartamento] = useState("");
+  const [Cargo, setCargo] = useState("");
+  const [Fecha_Ingreso, setFecha_Ingreso] = useState("");
+  const [Antiguedad, setAntiguedad] = useState(0);
 
-  const add =()=>{
-    axios.post("http://localhost:3001/create",{
-    nombre:Nombre,
-    edad: Edad,
-    departamento: Departamento,
-    cargo:Cargo,
-    fecha_ingreso:Fecha_Ingreso,
-    antiguedad:Antiguedad,
-    }).then(()=>{
+  const [empleadosList, setEmpleados] = useState([]);
+
+  useEffect(() => {
+    getEmpleados();
+  }, []);
+
+  const add = () => {
+    axios.post("http://localhost:3001/create", {
+      nombre: Nombre,
+      edad: Edad,
+      departamento: Departamento,
+      cargo: Cargo,
+      fecha_ingreso: Fecha_Ingreso,
+      antiguedad: Antiguedad,
+    }).then(() => {
       alert("Empleado Registrado");
+      getEmpleados();
     });
   }
 
-return (
-    <div className="App">
-      <div className= "datos">
-        <label>Nombre: <input 
-        onChange={(event)=>{
+  const getEmpleados = () => {
+    axios.get("http://localhost:3001/empleados").then((response) => {
+      setEmpleados(response.data);
+    });
+  }
+
+  return (
+    <div className="container">
+    <div className="datos"></div>
+   
+      <div className="card text-center">
+  <div className="card-header">
+    MAESTRO DE EMPLEADOS
+  </div>
+  <div className="card-body">
+
+    <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">Nombre: </span>
+        <input type="text" 
+        onChange={(event) => {
           setNombre(event.target.value);
         }}
-        type  ="text"/></label>
+        className="form-control" placeholder="Ingrese su Nombre" aria-label="Username" aria-describedby="basic-addon1"/>
+    </div>
 
-        <label>Edad: <input 
-        onChange={(event)=>{
+    <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">Edad: </span>
+        <input type="text" 
+        onChange={(event) => {
           setEdad(event.target.value);
         }}
-        type  ="text"/></label>
-        <label>Departamento: <input
-        onChange={(event)=>{
-          setDepartamento(event.target.value);
-        }}
-        type  ="text"/></label>
-        <label>Cargo: <input
-        onChange={(event)=>{
-          setCargo(event.target.value);
-        }}
-        type  ="text"/></label>
-        <label>Fecha_Ingreso: <input
-        onChange={(event)=>{
-          setfecha_Ingreso(event.target.value);
-        }}
-        type  ="date"/></label>
-        <label>Antiguedad: <input
-        onChange={(event)=>{
-          setAntiguedad(event.target.value);
-        }}
-        type  ="number"/></label>
-        <button onClick={add}>Registrar</button>
-      </div>
+        className="form-control" placeholder="Ingrese su Edad" aria-label="Username" aria-describedby="basic-addon1"/>
+    </div>
+
+    <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">Departamento: </span>
+        <input type="text" 
+          onChange={(event) => {
+            setDepartamento(event.target.value);
+          }}
+        className="form-control" placeholder="Ingrese su Departamento" aria-label="Username" aria-describedby="basic-addon1"/>
+    </div>
+
+    <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">Cargo: </span>
+        <input type="text" 
+          onChange={(event) => {
+            setCargo(event.target.value);
+          }}
+        className="form-control" placeholder="Ingrese su Cargo" aria-label="Username" aria-describedby="basic-addon1"/>
+    </div>
+
+    <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">Fecha_Ingreso: </span>
+        <input type="date" 
+              onChange={(event) => {
+                setFecha_Ingreso(event.target.value);
+              }}
+        className="form-control" placeholder="Ingrese su Cargo" aria-label="Username" aria-describedby="basic-addon1"/>
+    </div>
+
+    <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">Antiguedad: </span>
+        <input type="number" 
+            onChange={(event) => {
+              setAntiguedad(event.target.value);
+            }}
+        className="form-control" placeholder="Ingrese su Antiguedad" aria-label="Username" aria-describedby="basic-addon1"/>
+    </div>
+
+     <p className="card-text">
+      
+     </p>
+    <a href="Registrar"><button className="btn btn-primary" onClick={add}>Registrar</button></a>
+  </div>
+  
+</div>
+
+  <table className="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Edad</th>
+      <th scope="col">Departamento</th>
+      <th scope="col">Cargo</th>
+      <th scope="col">Fecha de Ingreso</th>
+      <th scope="col">Antiguedad</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      empleadosList.map((val,key)=>{
+        return <tr key={val.id}>
+                <th>{val.id}</th>
+                <td>{val.nombre}</td>
+                <td>{val.edad}</td>
+                <td>{val.departamento}</td>
+                <td>{val.cargo}</td>
+                <td>{val.fecha_ingreso}</td>
+                <td>{val.antiguedad}</td>
+               </tr>
+      })
+    }
+    
+  </tbody>
+  </table>
+ 
+  <div className="card-footer text-body-secondary">
+    Elaborado Por: Maycol Bautista
+  </div>
+
     </div>
   );
 }
--- final/server/index.js
+++ final/server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const mysql =require("mysql")
+const mysql = require("mysql");
 const cors = require("cors");
 
 app.use(cors());
@@ -13,6 +13,14 @@ const db = mysql.createConnection({
     database: "empleados_final"
 });
 
+db.connect((err) => {
+    if (err) {
+        console.error("Error al conectar con la base de datos: " + err.message);
+    } else {
+        console.log("Conexión exitosa a la base de datos");
+    }
+});
+
 app.post("/create", (req, res) => {
     const nombre = req.body.nombre;
     const edad = req.body.edad;
@@ -22,14 +30,27 @@ app.post("/create", (req, res) => {
     const antiguedad = req.body.antiguedad;
 
     db.query("INSERT INTO empleados (nombre, edad, departamento, cargo, fecha_ingreso, antiguedad) VALUES (?, ?, ?, ?, ?, ?)",
-    [nombre, edad, departamento, cargo, fecha_ingreso, antiguedad], (err, result) => {
+        [nombre, edad, departamento, cargo, fecha_ingreso, antiguedad], (err, result) => {
             if (err) {
+                console.log(err);
+                res.status(500).json({ error: "Error al registrar el empleado" });
+            } else {
+                res.status(200).json({ message: "Empleado Registrado con Éxito" });
+            }
+        });
+});
+
+app.get("/empleados", (req, res) => {
+    db.query("SELECT * FROM empleados", (err, result) => {
+        if (err) {
             console.log(err);
+            res.status(500).json({ error: "Error al obtener la lista de empleados" });
         } else {
-            res.send("Empleado Registrado con Éxito");
+            res.status(200).json(result);
         }
     });
 });
+
 app.listen(3001, () => {
     console.log("Corriendo en puerto 3001");
 });
