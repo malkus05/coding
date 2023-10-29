e8c0c1c0dad806cf39726e2bd8ba2162f3f479fb
malkus05 <maicol.bautista@gmail.com>
2023-10-29 01:40:32 -0500

    update index.js

--- final/server/index.js
+++ final/server/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-import { PORT } from './config.js';
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
@@ -7,19 +6,11 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-import {
-DB_HOST,
-DB_NAME,
-DB_PASSWORD,
-DB_PORT,
-DB_USER,
-} from "./config.js"
-
 const db = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "empleados_final"
 });
 
 db.connect((err) => {
@@ -35,11 +26,11 @@ app.post("/create", (req, res) => {
     const edad = req.body.edad;
     const departamento = req.body.departamento;
     const cargo = req.body.cargo;
-    const fecha_ingreso = req.body.fecha_ingreso;
+    const perfil = req.body.perfil;
     const antiguedad = req.body.antiguedad;
 
-    db.query("INSERT INTO empleados (nombre, edad, departamento, cargo, fecha_ingreso, antiguedad) VALUES (?, ?, ?, ?, ?, ?)",
-        [nombre, edad, departamento, cargo, fecha_ingreso, antiguedad], (err, result) => {
+    db.query("INSERT INTO empleados (nombre, edad, departamento, cargo, perfil, antiguedad) VALUES (?, ?, ?, ?, ?, ?)",
+        [nombre, edad, departamento, cargo, perfil, antiguedad], (err, result) => {
             if (err) {
                 console.log(err);
                 res.status(500).json({ error: "Error al registrar el empleado" });
@@ -50,8 +41,9 @@ app.post("/create", (req, res) => {
 });
 
 app.get("/empleados", (req, res) => {
-    db.query("SELECT * FROM empleados", (err, result) => {
-        if (err) {
+    db.query("SELECT * FROM empleados", 
+    (err, result)=>{
+        if(err){
             console.log(err);
             res.status(500).json({ error: "Error al obtener la lista de empleados" });
         } else {
@@ -61,43 +53,55 @@ app.get("/empleados", (req, res) => {
 });
 
 app.put("/update", (req, res) => {
-    const nombre = req.body.id;
+    const id = req.body.id;
+    const nombre = req.body.nombre;
     const edad = req.body.edad;
     const departamento = req.body.departamento;
     const cargo = req.body.cargo;
-    const fecha_ingreso = req.body.fecha_ingreso;
+    const perfil = req.body.perfil; // Asegúrate de que estás recibiendo 'perfil' correctamente
     const antiguedad = req.body.antiguedad;
 
-    db.query("UPDATE empleados SET nombre=?, edad=?, departamento=?, cargo=?, fecha_ingreso=?, antiguedad=? WHERE id=?", [nombre, edad, departamento, cargo, fecha_ingreso, antiguedad, id], 
-         (err, result) => {
-            if (err) {
-                console.log(err);
-                res.status(500).json({ error: "Error al Actualizar el empleado" });
-            } else {
-                res.status(200).json({ message: "Empleado Actualizado con Éxito" });
-            }
-        });
+    if (!id || isNaN(id)) {
+        res.status(400).json({ error: "ID no válido" });
+        return;
+    }
+
+    const acualizaQuery = "UPDATE empleados SET nombre=?, edad=?, departamento=?, cargo=?, perfil=?, antiguedad=? WHERE id = ?";
+    const actualizaValores = [nombre, edad, departamento, cargo, perfil, antiguedad, id];
+
+    db.query(acualizaQuery, actualizaValores, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: "Error al Actualizar el empleado" });
+        } else {
+            res.status(200).json({ message: "Empleado Actualizado con Éxito" });
+        }
+    });
 });
+
 
 app.delete("/delete/:id", (req, res) => {
     const id = req.params.id;
-    db.query("DELETE FROM empleados WHERE id=?", id, 
-         (err, result) => {
-            if (err) {
-                console.log(err);
-                res.status(500).json({ error: "Error al Eliminar el empleado" });
-            } else {
-                res.status(200).json({ message: "Empleado Eliminado con Éxito" });
-            }
-        });
+
+    if (!id || isNaN(id)) {
+        return res.status(200).json({ error: "ID no válido" });
+    }
+
+    db.query("DELETE FROM empleados WHERE id = ?", id, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ error: "Error al Eliminar el empleado" });
+        }
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: "ID no encontrado" });
+        }
+
+        return res.status(200).json({ message: "Empleado Eliminado con Éxito" });
+    });
 });
 
-<<<<<<< HEAD
 
-app.listen(3001,()=>{
-    console.log(`Escuchando en puerto 3001`)
-=======
 app.listen(3001, () => {
     console.log("Corriendo en puerto 3001");
->>>>>>> parent of df0b50c (Full stack - pendiente delete)
 });
